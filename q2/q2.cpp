5d543b725b49f33d68e0b396e3d67f69f1259785
dannytoy <chiisaidanny@gmail.com>
2023-11-30 14:53:02 -0700

    q2_finished

--- q2/q2.cpp
+++ q2/q2.cpp
@@ -1,8 +1,23 @@
+// CSCI 1300 Fall 2023
+// Author: Danny Toy
+// TA: Nick Cooper
+// Question 2
+
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double calculateSigmoid(double x)
+{
+    return (1/(1+(exp(-x))));
+}
+
 int main()
 {
-    
+    double x;
+    cout << "Enter a value for x:" << endl;
+    cin >> x;
+    cout << "The sigmoid for x=" << x << " is " << calculateSigmoid(x) << endl;
+    return 0;
 }
