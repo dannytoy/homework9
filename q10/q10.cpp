0c46d85894e250a4714e297e73dc198de8693abf
dannytoy <chiisaidanny@gmail.com>
2023-12-09 19:30:53 -0700

    q10_finished

--- q10/q10.cpp
+++ q10/q10.cpp
@@ -26,30 +26,64 @@ bool insertAfter(char input_characters[], int num_elements, int ARR_SIZE, int in
 
 int insertConsonants(char input_characters[], int num_elements, int ARR_SIZE, int num_consonants)
 {
+    int new_num_elements = num_elements;
     if(num_consonants + num_elements > ARR_SIZE)
     {
         return num_elements;
     }
     else
     {
-        for(int i = 0; i < num_elements; i++)
+        for(int i = 0; i < (num_elements + num_consonants); i++)
         {
-            if(input_characters[i] != 'a' && input_characters[i] != 'e' && input_characters[i] != 'i' && input_characters[i] != 'o' && input_characters[i] != 'u')
+            if(isupper(input_characters[i]))
+            {
+                continue;
+            }
+            else if(input_characters[i] != 'a' && input_characters[i] != 'e' && input_characters[i] != 'i' && input_characters[i] != 'o' && input_characters[i] != 'u')
             {
                 char char_to_insert = input_characters[i] - 32;
-                insertAfter(input_characters, num_elements, ARR_SIZE, i, char_to_insert);
+                insertAfter(input_characters, new_num_elements, ARR_SIZE, i, char_to_insert);
+                new_num_elements++;
             }
         }
-        return num_elements + num_consonants;
+        return new_num_elements;
     }
 }
 
 int main()
 {
-    const int ARR_SIZE = 5;
+    /* const int ARR_SIZE = 5;
     char input_characters[ARR_SIZE] = "cat";
     int num_elements = 3;
     int num_consonants = 2;
+    // updating num_elements with the updated value returned by insertConsonants
+    num_elements = insertConsonants(input_characters, num_elements, ARR_SIZE, num_consonants);
+    // print num_elements
+    cout << "Function returned value: " << num_elements << endl;
+    // print array contents
+    for(int i = 0; i < ARR_SIZE; i++)
+    {
+        cout << input_characters[i];
+    }  */
+
+    /* const int ARR_SIZE = 12;
+    char input_characters[ARR_SIZE] = "boulder";
+    int num_elements = 7;
+    int num_consonants = 4;
+    // updating num_elements with the updated value returned by insertConsonants
+    num_elements = insertConsonants(input_characters, num_elements, ARR_SIZE, num_consonants);
+    // print num_elements
+    cout << "Function returned value: " << num_elements << endl;
+    // print array contents
+    for(int i = 0; i < ARR_SIZE; i++)
+    {
+        cout << input_characters[i];
+    }  */
+
+    const int ARR_SIZE = 9;
+    char input_characters[ARR_SIZE] = "apples";
+    int num_elements = 6;
+    int num_consonants = 4;
     // updating num_elements with the updated value returned by insertConsonants
     num_elements = insertConsonants(input_characters, num_elements, ARR_SIZE, num_consonants);
     // print num_elements
